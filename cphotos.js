e00cf2ef5da7749f87d96932e34b8b0a59c29806
whoisterencelee <whoisterencelee@hotmail.com>
2024-07-11 11:13:20 +0800

    cleaned up carousel

---
+++ cphotos.js
@@ -0,0 +1,9 @@
+var cphotos = [
+	{ src : "assets/raisehandandparticipate.png" , comment : "Engage participation" } ,
+	{ src : "assets/girlreadingbook.png" , comment : "Accumulate knowledge" } ,
+	{ src : "assets/researchboy.png" , comment : "Discover insights" } ,
+	{ src : "assets/groupfeedback.png" , comment : "Analytical approach" } ,
+	{ src : "assets/wealthteacher.png" , comment : "Motivation driven" } ,
+	/* add links here */
+
+	]
