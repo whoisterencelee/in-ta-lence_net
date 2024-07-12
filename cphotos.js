74271026417abe05c83ca249d295c83c571f1c86
Terence Lee <whoisterencelee@hotmail.com>
2024-07-12 09:06:37 +0800

    Update from intalence.net/cphotos.js/add_to_cphotos.js

--- cphotos.js
+++ cphotos.js
@@ -4,6 +4,7 @@ var cphotos = [
 	{ src : "assets/researchboy.png" , comment : "Discover insights" } ,
 	{ src : "assets/groupfeedback.png" , comment : "Analytical approach" } ,
 	{ src : "assets/wealthteacher.png" , comment : "Motivation driven" } ,
+	{ src : "https://sustaincia.org/logo/sustaincia_org_1024x1024.png" , comment : "Partners support" } ,
 	/* add links here */
 
 	]
