41630d1764f7602a87fe19cbd0d53a25c7d5dc22
Terence Lee <whoisterencelee@hotmail.com>
2024-07-12 09:12:19 +0800

    reorder photo adding

--- cphotos.js
+++ cphotos.js
@@ -1,10 +1,10 @@
 var cphotos = [
+	/* add photo here */
 	{ src : "assets/raisehandandparticipate.png" , comment : "Engage participation" } ,
 	{ src : "assets/girlreadingbook.png" , comment : "Accumulate knowledge" } ,
 	{ src : "assets/researchboy.png" , comment : "Discover insights" } ,
 	{ src : "assets/groupfeedback.png" , comment : "Analytical approach" } ,
 	{ src : "assets/wealthteacher.png" , comment : "Motivation driven" } ,
 	{ src : "https://sustaincia.org/logo/sustaincia_org_1024x1024.png" , comment : "Partners support" } ,
-	/* add links here */
 
 	]
